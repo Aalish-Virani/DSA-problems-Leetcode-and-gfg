301df6b60a9f919499bfe73d47b4d0f2b34d666d
Aalish Virani <126311156+Aalish-Virani@users.noreply.github.com>
2023-07-05 00:04:53 +0500

    Time: 3 ms (47.00%), Space: 8.1 MB (82.78%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        short temp;
+        short count = nums.size()-1;
+        
+        while(count>0){
+
+            for(int j=0; j<count; j++){
+            
+                if (nums[j] >= nums[j+1]){
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+                
+            }
+            
+            count--;
+        }
+        
+        for (count = 0; count < nums.size(); count++){
+            cout<<nums[count];
+        }
+    }
+};
